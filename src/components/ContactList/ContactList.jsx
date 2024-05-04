c8f3ebf5ed472cd6a97afaf1fe32a563907a9a6d
AnnaHtslk <hannah.htslk@gmail.com>
2024-05-04 12:53:09 +0300

    finished styling contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,8 +3,8 @@ import s from "./ContactList.module.css";
 
 const ContactList = ({ contacts }) => {
   return (
-    <div>
-      <ul>
+    <div className={s.container}>
+      <ul className={s.list}>
         {contacts.map((contact) => {
           return (
             <li className={s.item} key={contact.id}>
